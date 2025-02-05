f262c1a89c80719c6831be3c701fde1b1cb78ce7
Rodrigo <rodrigolopes.92@hotmail.com>
2025-02-05 10:03:21 -0300

    Resultado-Escolar

---
+++ resultado-escolar/src/ResultadoEscolar.java
@@ -0,0 +1,16 @@
+public class ResultadoEscolar {
+    
+    public static void main(String[] args){
+        int nota = 6;
+        if(nota >=7)
+        System.out.println("Aprovado!");
+
+        else if (nota >5 && nota<7)
+        System.out.println("Aluno em recuperação");
+
+      
+        else System.out.println("Reprovado!");
+
+
+    }
+}
