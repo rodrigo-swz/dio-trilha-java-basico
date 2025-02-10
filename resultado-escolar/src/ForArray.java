76509af8ce4070847fd21710c6b0a3cdb60f6da0
Rodrigo <rodrigolopes.92@hotmail.com>
2025-02-10 08:29:37 -0300

    For com length

---
+++ resultado-escolar/src/ForArray.java
@@ -0,0 +1,12 @@
+public class ForArray {
+    
+    public static void main(String[] args) {
+        String alunos [] = {"Felipe", "Jonas", "Julia", "Marcos"};
+
+        for (int x=0; x<alunos.length; x++){
+            System.out.println("O aluno no índice x=" + x + " é " + alunos[x]);
+        }
+    }
+
+
+}
