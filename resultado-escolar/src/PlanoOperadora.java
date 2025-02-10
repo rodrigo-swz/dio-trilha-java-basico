ce0ed684daa5d0da7251587024f450167125d7d9
Rodrigo <rodrigolopes.92@hotmail.com>
2025-02-10 08:00:17 -0300

    Utilizando o switch/case sem o break;

---
+++ resultado-escolar/src/PlanoOperadora.java
@@ -0,0 +1,22 @@
+public class PlanoOperadora {
+    
+    public static void main(String[] args){
+        String plano = "C";
+
+        switch (plano){
+            case "A": {
+                System.out.println("5GB de youtube");
+
+            }
+            case "B": {
+                System.out.println("WhatsApp ilimitado");
+
+            }
+            case "C": {
+                System.out.println("100 minutos de ligação");
+
+            }
+        }
+
+    }
+}
