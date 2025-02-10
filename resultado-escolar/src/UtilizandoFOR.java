f6370d84f4d8180f6e2ee59b8320d83a4adbf4ca
Rodrigo <rodrigolopes.92@hotmail.com>
2025-02-10 08:19:45 -0300

    Utilizando o FOR

---
+++ resultado-escolar/src/UtilizandoFOR.java
@@ -0,0 +1,8 @@
+public class UtilizandoFOR {
+    
+    public static void main(String[] args) {
+        for (int carneirinhos = 0; carneirinhos <=10; carneirinhos ++) {
+            System.out.println("Contando carneirinhos, atualmente: " + carneirinhos);
+        }
+    }
+}
